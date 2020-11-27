66b374aab90a4babd35b4a01c06bcb1c867afa9c
SOONWOOK PARK <soonwook34@gmail.com>
2020-11-27 21:31:00 +0900

    S_sendServerData 검색 기능 추가중

--- S_Server/S_sendServerData.py
+++ S_Server/S_sendServerData.py
@@ -1,11 +1,71 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask("Hello World!")
+
+levelDict = {1: {"1": "접촉안내", "2": "동선공개", "3": "발생안내", "9": "캠페인"},
+                2: {"1": "지진", "9": "기타"},
+                3: {"1": "경보", "2": "주의보", "9": "저감조치"},
+                4: {"1": "경보", "2": "주의보", "9": "조치조치"},
+                5: {"1": "경보", "2": "주의보", "9": "조치알림"},
+                6: {"1": "경보", "2": "주의보", "9": "기타"},
+                7: {"1": "경보", "2": "주의보", "9": "기타"},
+                8: {"1": "경보", "2": "주의보", "9": "기타"},
+                9: {"1": "경보", "2": "주의보", "9": "기타"}}
 
 @app.route("/")
 def home():
     print("[S_sendServerData] REST request : '/'")
     return render_template("home.html")
+
+@app.route("/search")
+def search():
+    # 공통
+    start_date = request.args.get("start_date")
+    end_date = request.args.get("end_date")
+    main_location = request.args.get("main_location")
+    sub_location = request.args.get("sub_location")
+    disaster = request.args.get("disaster")
+    level = request.args.get("level")
+    inner_text = request.args.get("inner_text")
+
+    # 전염병
+    if disaster == 1:
+        name = request.args.get("name")
+        levels = level.split(",")
+        req_level = []
+        for level in levels:
+            req_level.append(levelDict[level])
+        if inner_text:
+            print(f"[S_sendServerData]{disaster} {name} {req_level}/{start_date}~{end_date}/{main_location} {sub_location}/{inner_text}")
+        else:
+            print(f"[S_sendServerData]{disaster} {name} {req_level}/{start_date}~{end_date}/{main_location} {sub_location}")
+        
+        return render_template("home.html")
+    # 지진
+    elif disaster == 2:
+        return render_template("home.html")
+    # 미세먼지
+    elif disaster == 3:
+        return render_template("home.html")
+    # 태풍
+    elif disaster == 4:
+        return render_template("home.html")
+    # 홍수
+    elif disaster == 5:
+        return render_template("home.html")
+    # 폭염
+    elif disaster == 6:
+        return render_template("home.html")
+    # 한파
+    elif disaster == 7:
+        return render_template("home.html")
+    # 호우
+    elif disaster == 8:
+        return render_template("home.html")
+    # 대설
+    else:
+        return render_template("home.html")
+
 
 
 app.run(host="203.253.25.184", port=8080)
