59340a7a2a9175f82976b41d1009ca5020cd20c4
soonwook34 <soonwook34@gmail.com>
2020-11-29 23:02:27 +0900

    S_sendServerData search 구성 완료

--- S_Server/S_sendServerData.py
+++ S_Server/S_sendServerData.py
@@ -29,7 +29,7 @@ disasterDict = {1: "전염병", 2: "지진", 3: "미세먼지", 4: "태풍", 5: 
 levelDict = {1: {1: "접촉안내", 2: "동선공개", 3: "발생안내", 9: "캠페인"},
                 2: {1: "지진", 9: "기타"},
                 3: {1: "경보", 2: "주의보", 9: "저감조치"},
-                4: {1: "경보", 2: "주의보", 9: "조치조치"},
+                4: {1: "경보", 2: "주의보", 9: "조치알림"},
                 5: {1: "경보", 2: "주의보", 9: "조치알림"},
                 6: {1: "경보", 2: "주의보", 9: "기타"},
                 7: {1: "경보", 2: "주의보", 9: "기타"},
@@ -57,7 +57,7 @@ app = Flask("SoonItSoon Server")
 @app.route("/")
 def home():
     now_date = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
-    print(f"[{now_date} S_sendServerData] REST connection request")
+    print(f"{now_date} [S_sendServerData] REST connection request")
     return render_template("home.html")
 
 # 재난문자 검색
@@ -86,13 +86,17 @@ def search():
     inner_text = request.args.get("inner_text")
 
     # AM 테이블 쿼리
-    sql_AMall = f"SELECT * FROM AM WHERE (send_date BETWEEN '{start_date}' AND '{end_date}' AND disaster = {disaster})"
+    sql_AMall = f"SELECT * FROM AM WHERE (send_date BETWEEN '{start_date}' AND '{end_date}') AND disaster = {disaster}"
     if inner_text:
         sql_AMall += f" AND msg like '%{inner_text}%'"
-    sql_AMloc = f"{sql_AMall} AND (send_location LIKE '%{main_location} {sub_location}%' OR send_location LIKE '%{main_location} 전체%')"
+    if sub_location == "전체":
+        sql_AMloc = f"{sql_AMall} AND (send_location LIKE '%{main_location}%')"
+    else:
+        sql_AMloc = f"{sql_AMall} AND (send_location LIKE '%{main_location} {sub_location}%' OR send_location LIKE '%{main_location} 전체%')"
     # SQL 쿼리와 로그
     sql = ""
-    log = f"[{now_date} S_sendServerData] REST search request\n"
+    log_default = f"{now_date} [S_sendServerData]"
+    log = f"{log_default} REST search request\n"
 
     # 전염병(1) 태풍(4)
     if disaster in [1, 4]:
@@ -100,13 +104,13 @@ def search():
         name = request.args.get("name")
 
         sql_multi = f"SELECT * FROM {AlertMsgDBDict[disaster]} WHERE name = '{name}' AND level IN ({level})"
-        log += f"disaster : {disasterDict[disaster]} {name}\nlevel : {levels}\ndate : {start_date} ~ {end_date}\n"
+        log += f"{log_default} disaster : {disasterDict[disaster]} {name}\n{log_default} level : {levels}\n{log_default} date : {start_date} ~ {end_date}\n"
         if main_location and sub_location:
             sql = f"SELECT * FROM ({sql_AMloc}) AS AM JOIN ({sql_multi}) AS {AlertMsgDBDict[disaster]} USING (mid)"
-            log += f"location : {main_location} {sub_location}\n"
+            log += f"{log_default} location : {main_location} {sub_location}\n"
         else:
             sql = f"SELECT * FROM ({sql_AMall}) AS AM JOIN ({sql_multi}) AS {AlertMsgDBDict[disaster]} USING (mid)"
-            log += "location : 전체\n"
+            log += f"{log_default} location : 전체\n"
     # 지진(2)
     elif disaster == 2:
         # 최소/최대 규모
@@ -116,28 +120,28 @@ def search():
         obs_location = request.args.get("obs_location")
 
         sql_EQ = f"SELECT * FROM EQ WHERE level IN ({level}) AND (scale BETWEEN {scale_min} AND {scale_max} OR scale IS NULL)"
-        log += f"disaster : {disasterDict[disaster]}\nlevel : {levels}\ndate : {start_date} ~ {end_date}\n"
+        log += f"{log_default} disaster : {disasterDict[disaster]}\n{log_default} level : {levels}\n{log_default} date : {start_date} ~ {end_date}\n"
         if main_location and sub_location:
             sql = f"SELECT * FROM ({sql_AMloc}) AS AM JOIN ({sql_EQ}) AS EQ USING (mid)"
-            log += f"location : {main_location} {sub_location}\n"
+            log += f"{log_default} location : {main_location} {sub_location}\n"
         else:
             sql = f"SELECT * FROM ({sql_AMall}) AS AM JOIN ({sql_EQ}) AS EQ USING (mid)"
-            log += "location : 전체\n"
+            log += f"{log_default} location : 전체\n"
         if obs_location:
             sql += f" WHERE EQ.obs_location = '{obs_location}' OR EQ.obs_location IS NULL"
-            log += f"scale : {scale_min} ~ {scale_max}\nobs_location : {obs_location}\n"
+            log += f"{log_default} scale : {scale_min} ~ {scale_max}\n{log_default} obs_location : {obs_location}\n"
         else:
-            log += f"scale : {scale_min} ~ {scale_max}\nobs_location : 전체\n"
+            log += f"{log_default} scale : {scale_min} ~ {scale_max}\n{log_default} obs_location : 전체\n"
     # 미세먼지(3) 홍수(5) 폭염(6) 한파(7) 호우(8) 대설(9)
     elif disaster in [3, 5, 6, 7, 8, 9]:
         sql_multi = f"SELECT * FROM {AlertMsgDBDict[disaster]} WHERE level IN ({level})"
-        log += f"disaster : {disasterDict[disaster]}\nlevel : {levels}\ndate : {start_date} ~ {end_date}\n"
+        log += f"{log_default} disaster : {disasterDict[disaster]}\n{log_default} level : {levels}\n{log_default} date : {start_date} ~ {end_date}\n"
         if main_location and sub_location:
             sql = f"SELECT * FROM ({sql_AMloc}) AS AM JOIN ({sql_multi}) AS {AlertMsgDBDict[disaster]} USING (mid)"
-            log += f"location : {main_location} {sub_location}\n"
+            log += f"{log_default} location : {main_location} {sub_location}\n"
         else:
             sql = f"SELECT * FROM ({sql_AMall}) AS AM JOIN ({sql_multi}) AS {AlertMsgDBDict[disaster]} USING (mid)"
-            log += "location : 전체\n"
+            log += f"{log_default} location : 전체\n"
     # # 태풍(4)
     # elif disaster == 4:
     #     name = request.args.get("name")
@@ -159,21 +163,18 @@ def search():
     #     print("대설")
     
     if inner_text:
-        log += f"inner_text : {inner_text}\n"
+        log += f"{log_default} inner_text : {inner_text}\n"
     else:
-        log += "inner_text : none\n"
+        log += f"{log_default} inner_text : none\n"
     sql += " ORDER BY AM.mid DESC LIMIT 100;"
-    log += f"DB query : {sql}"
-    if disaster not in (1, 2):
-        sql = f"SELECT * FROM alertMsg WHERE date BETWEEN '{start_date}' AND '{end_date}';"
+    log += f"{log_default} DB query : {sql}\n"
     AlertMsgDB_cursor.execute(sql)
     result = AlertMsgDB_cursor.fetchall()
     jsonAll = dict(zip(range(1, len(result) + 1), result))
 
+    log += f"{log_default} DB result : {len(result)} results"
     print(log)
-    # all_data = {"log_data": json.dumps(log, default=json_default, ensure_ascii=False), "db_data": json.dumps(jsonAll, default=json_default, ensure_ascii=False)}
     return render_template("search.html", all_data=json.dumps(jsonAll, default=json_default, ensure_ascii=False))
-# return render_template("search.html", all_data=json.dumps(all_data, ensure_ascii=False))
 
 # 서버 run
 app.run(host="203.253.25.184", port=8080)
