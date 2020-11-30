b695cf5950c2b3b43256edc3ba6672846e1e4a1d
soonwook34 <soonwook34@gmail.com>
2020-11-30 15:12:51 +0900

    S_sendServerData 서버 변수 복구

--- soonitsoon_server_main.py
+++ soonitsoon_server_main.py
@@ -15,12 +15,3 @@ import S_Background.S_mainBackground
 # S_Server.S_sendServerData 모듈
 import S_Server.S_sendServerData
 
-# AlertMsgDB 접근 변수
-AlertMsgDB = pymysql.connect(
-     user='kyeol',
-     passwd='hee',
-     host='127.0.0.1',
-     db='AlertMsgDB',
-     charset='utf8'
-)
-AlertMsgDB_cursor = AlertMsgDB.cursor(pymysql.cursors.DictCursor)
