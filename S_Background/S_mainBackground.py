a1ecadbfeeb8f604bda76c9b024ff07d6cf3e456
SOONWOOK PARK <soonwook34@gmail.com>
2020-11-30 15:15:37 +0900

    S_mainBackground add time on logging

--- S_Background/S_mainBackground.py
+++ S_Background/S_mainBackground.py
@@ -1,12 +1,14 @@
 import threading
-
+import time
 count = 0
 
 def startTimer():
     # count 변수 사용 
     global count
     count += 1
-    print(f"[S_mainBackground] {count} minutes ON..")
+    now_date = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
+    log_default = f"{now_date} [S_mainBackground]"
+    print(f"{log_default} {count} minutes ON..")
     timer = threading.Timer(60, startTimer)
     timer.start()
 
